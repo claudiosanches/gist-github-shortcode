0948a90422a8caf7558a5b7f9a9f626f38ae245e
Claudio Sanches <contato@claudiosmweb.com>
2013-01-04 00:05:47 -0200

    Source code reformulation

--- gist-shortcode.php
+++ gist-shortcode.php
@@ -3,62 +3,76 @@
  * Plugin Name: Gist Shortcode
  * Plugin URI: http://claudiosmweb.com/plugins/gist-shortcode-wordpress-plugin/
  * Description: Adds Github Gists in your posts via shortcode.
- * Version: 1.0
+ * Version: 1.1
  * Author: Claudio Sanches
  * Author URI: http://claudiosmweb.com/
  * License: GPLv2 or later
  */
 
-/**
- * Gist shortcode.
- *
- * Usage:
- * [gist id="Gist ID" file="Gist File (optional)"]
- */
-function gistscode_shortcode( $atts ) {
-    extract( shortcode_atts( array(
-        'id' => '',
-        'file' => '',
-    ), $atts ) );
+class Gist_Github_Shortcode {
 
-    $gist = sprintf(
-        '<script src="https://gist.github.com/%s.js%s"></script>',
-        esc_attr( $id ),
-        $file ? '?file=' . esc_attr( $file ) : ''
-    );
+    /**
+     * Class construct.
+     */
+    public function __construct() {
 
-    return $gist;
-}
-add_shortcode( 'gist', 'gistscode_shortcode');
+        // Register the shortcode.
+        add_shortcode( 'gist', array( &$this, 'shortcode' ) );
 
-
-/**
- * Add custom buttons in TinyMCE.
- */
-function gistscode_register_buttons( $buttons ) {
-    array_push( $buttons, '|', 'gist' );
-    return $buttons;
-}
-
-/**
- * Register button scripts.
- */
-function gistscode_add_buttons( $plugin_array ) {
-    $plugin_array['gist'] = plugins_url( 'tinymce/gist.js' , __FILE__ );
-    return $plugin_array;
-}
-
-/**
- * Register buttons in init.
- */
-function gistscode_buttons_init() {
-    if ( !current_user_can( 'edit_posts' ) && !current_user_can( 'edit_pages' ) ) {
-        return;
+        // Actions.
+        add_action( 'init', array( &$this, 'buttons_init' ) );
     }
 
-    if ( get_user_option( 'rich_editing') == 'true' ) {
-        add_filter( 'mce_external_plugins', 'gistscode_add_buttons' );
-        add_filter( 'mce_buttons', 'gistscode_register_buttons' );
+    /**
+     * Gist shortcode.
+     *
+     * Usage:
+     * [gist id="Gist ID" file="Gist File (optional)"]
+     */
+    function shortcode( $atts ) {
+        extract( shortcode_atts( array(
+            'id' => '',
+            'file' => '',
+        ), $atts ) );
+
+        $gist = sprintf(
+            '<script src="https://gist.github.com/%s.js%s"></script>',
+            esc_attr( $id ),
+            $file ? '?file=' . esc_attr( $file ) : ''
+        );
+
+        return $gist;
     }
-}
-add_action( 'init', 'gistscode_buttons_init' );
+
+    /**
+     * Add custom buttons in TinyMCE.
+     */
+    function register_buttons( $buttons ) {
+        array_push( $buttons, '|', 'gist' );
+        return $buttons;
+    }
+
+    /**
+     * Register button scripts.
+     */
+    function add_buttons( $plugin_array ) {
+        $plugin_array['gist'] = plugins_url( 'tinymce/gist.js' , __FILE__ );
+        return $plugin_array;
+    }
+
+    /**
+     * Register buttons in init.
+     */
+    function buttons_init() {
+        if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) {
+            return;
+        }
+
+        if ( true == get_user_option( 'rich_editing') ) {
+            add_filter( 'mce_external_plugins', array( &$this, 'add_buttons' ) );
+            add_filter( 'mce_buttons', array( &$this, 'register_buttons' ) );
+        }
+    }
+} // close Gist_Github_Shortcode class.
+
+$gist_github_shortcode = new Gist_Github_Shortcode;
